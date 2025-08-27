128e2e7fdd2500855d2806c7f84e634871afd43c
sunny-rp <sunnyprajapar65351@gmail.com>
2025-08-27 12:21:36 +0530

    Dashboard api done

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -23,4 +23,20 @@ router.post("/create", upload.single("image"), async (req, res) => {
   }
 })
 
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const productId = req.params.id
+    const deletedProduct = await productModel.findByIdAndDelete(productId)
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" })
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" })
+  } catch (error) {
+    console.error("Error deleting product:", error)
+    res.status(500).json({ error: "Error deleting product" })
+  }
+})
+
 module.exports = router
